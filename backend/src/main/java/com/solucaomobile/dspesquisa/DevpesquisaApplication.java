8ba90ca226bf83689e4a4c2203c3abc1af21c9ea
Jose Valderez <valderezpe@gmail.com>
2020-09-14 11:07:21 -0300

    Project created

---
+++ backend/src/main/java/com/solucaomobile/dspesquisa/DevpesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.solucaomobile.dspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevpesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevpesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/solucaomobile/dspesquisa/DevpesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solucaomobile.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevpesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
